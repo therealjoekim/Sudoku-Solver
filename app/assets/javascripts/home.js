9f2b9c34858f40659783b7be66e6f6d33ac97898
Claire Samuels <clairemation@gmail.com>
2016-04-01 18:06:03 -0400

    Ajax table

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready (function(event){
+
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    var request = $.ajax({
+      type: "POST",
+      url: $(this).attr("action"),
+      data: $(this).serialize()
+    });
+    request.done(function(response){
+      console.log("response: ", response);
+      for(var row=0; row < response.length; row++){
+        for(var col=0; col < response[row].length; col++){
+          var selector = "tr:nth-child(" + (row + 1) + ") td:nth-child(" + (col + 1) + ") input";
+          $($(selector)[0]).val(response[row][col]);
+        };
+      };
+    });
+  });
+
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,9 @@ class HomeController < ApplicationController
   def results
     sudoku_string = Solver.make_string(params)
     @answer = Solver.solve(sudoku_string)
+    if request.xhr?
+      render :json => @answer
+    end
   end
 
 end
