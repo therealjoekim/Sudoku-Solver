5999c83a86fe13fd6d8d57d1dd477b0001c22688
Joe Kim <joseph.joe.kim@gmail.com>
2016-05-13 21:19:38 -0400

    add timeout function and error alert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,23 @@ class HomeController < ApplicationController
 
   def results
     sudoku_string = Solver.make_string(params)
-    @answer = Solver.solve(sudoku_string)
-    if request.xhr?
-      render :json => @answer
+    # if sudoku cannot complete will timeout after 1 second
+    begin
+    	@result = Timeout::timeout(1) do
+		    @answer = Solver.solve(sudoku_string)
+	        if request.xhr?
+		    	render :json => @answer
+		    end
+    	end
+    rescue Timeout::Error
+    	# flash[:alert] = "The Sudoku Board is invalid. Please reset and try again."
+    	# puts "error"
+    	render js: "alert('The Sudoku Board is invalid. Please reset and try again.');"
+    	# redirect_to root_path
     end
+    # if request.xhr?
+    # 	render :json => @answer
+    # end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def alert_for(flash_type)
+    { error: 'alert-danger' }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
